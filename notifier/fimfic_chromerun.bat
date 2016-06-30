eb81997ff86cebc08034fc6e4689277b4cceba06
Ben Wilson <benjamin.jeremy.wilson@gmail.com>
2016-06-30 20:01:43 +0100

    remove some files

--- notifier/fimfic_chromerun.bat
+++
@@ -1,2 +0,0 @@
-echo off
-CALL "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Profile 1" "http://www.fimfiction.net"
--- notifier/new_chapter_chromerun.bat
+++
@@ -1,2 +0,0 @@
-echo off
-CALL "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Profile 1" "http://www.fimfiction.net/story/290208/176/utaan/everywhere-and-everytime-all-encompassing"
